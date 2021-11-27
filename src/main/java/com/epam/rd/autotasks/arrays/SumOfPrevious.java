bee23e1ddcc82261cb869ee56824380fe32ca1d1
Daniil Laureanti <daniil.laureanti@gmail.com>
2021-11-27 12:06:10 +0200

    Completed task

--- src/main/java/com/epam/rd/autotasks/arrays/SumOfPrevious.java
+++ src/main/java/com/epam/rd/autotasks/arrays/SumOfPrevious.java
@@ -4,16 +4,16 @@ import java.util.Arrays;
 
 public class SumOfPrevious {
 
-    public static void main(String[] args) {
-        int[] array = new int[]{1, -1, 0, 4, 6, 10, 15, 25};
-
-        System.out.println(Arrays.toString(getSumCheckArray(array)));
-    }
-
-    public static boolean[] getSumCheckArray(int[] array){
-
-        //put your code here
-
-        throw new UnsupportedOperationException();
+    public static boolean[] getSumCheckArray(int[] values) {
+        if (values == null || values.length < 2)
+            throw new IllegalArgumentException("Illegal argument exception input parameter is null or length less than 2");
+        boolean[] rets = new boolean[values.length];
+        for (int i = 0; i < values.length; i++) {
+            if (i <= 1)
+                rets[i] = false;
+            else
+                rets[i] = values[i] == (values[i-1] + values[i-2]);
+        }
+        return rets;
     }
 }
